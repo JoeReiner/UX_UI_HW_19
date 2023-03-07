61f0e57bd839fca3d594124d81b40b994070298d
JoeReiner <josephguyreiner@gmail.com>
2023-03-07 15:24:38 -0600

    Analytics Add

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log("your index.js file is loaded correctly!");
+
+$("button").hover(function(){
+    $(this).css("background-color","#4B7DF7");
+  },
+  function(){
+    $(this).css("background-color", "#1E57E4");
+    
+  });
+
+
+
+
+
